4c584a9523d84bfdea9b9b49baf46f40d34d7ac2
Sven Ulland <sven.ulland+github@xeneta.com>
2020-10-26 16:04:42 +0100

    Allow ListBucket for `aws s3 sync` command

--- iam.tf
+++ iam.tf
@@ -20,6 +20,16 @@ data "aws_iam_policy_document" "itcfy_web_s3_upload" {
 
   statement {
     actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.web.arn,
+    ]
+  }
+
+  statement {
+    actions = [
       "s3:*",
     ]
 
